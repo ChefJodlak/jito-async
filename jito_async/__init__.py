2690ce3e43fea06878bd7b9b557e5f5aff62b6f1
ChefJodlak <jodlasde@gmail.com>
2025-01-21 16:54:32 +0100

    Bumped version

--- jito_async/__init__.py
+++ jito_async/__init__.py
@@ -7,5 +7,5 @@ from jito_async.core.sdk import (
     JitoResponseError,
 )
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["JitoJsonRpcSDK", "JitoError", "JitoConnectionError", "JitoResponseError"] 
